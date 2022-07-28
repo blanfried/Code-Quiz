442e05b328c702485c39b0b884d27b1a87e9e0ec
Baer Lanfried <blanfried@yahoo.com>
2022-07-28 15:56:59 +0100

    Added questions to js file

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,3 +8,39 @@ let currentQuestion = {}
 let acceptingAnswers = true
 let score = 0
 let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+      {
+            question: 'What is \{ called?',
+            choice1: 'Bracket',
+            choice2: 'Curly bracket',
+            choice3: 'Comma',
+            choice4: 'Parentheses',
+            answer: 2,
+      },
+      {
+            question: 'What symbol signifies a class?',
+            choice1: '.',
+            choice2: '#',
+            choice3: ',',
+            choice4: '\'',
+            answer: 1,
+      },
+      {
+            question: 'What symbol is used to increment by 1?',
+            choice1: '--',
+            choice2: '==',
+            choice3: '||',
+            choice4: '++',
+            answer: 4,
+      },
+      {
+            question: 'What does "true" translate to if it is made a number?',
+            choice1: '1',
+            choice2: '0',
+            choice3: '2',
+            choice4: '-1',
+            answer: 1,
+      } 
+]
