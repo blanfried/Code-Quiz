b19995870db6b4702b8d1185b73aea05eb701561
Baer Lanfried <blanfried@yahoo.com>
2022-08-01 16:10:22 +0100

    Added 1 minute timer, when timer runs out, alert appears, when user clicks ok on alert the current score is registered as final score and quiz ends. Also changed some stylistic preferences, and solved the issue preventing correct answer from turning green

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,107 +11,128 @@ let questionCounter = 0
 let availableQuestions = []
 
 let questions = [
-      {
-            question: 'What is \"\{\" called?',
-            choice1: 'Bracket',
-            choice2: 'Curly bracket',
-            choice3: 'Comma',
-            choice4: 'Parentheses',
-            answer: 2,
-      },
-      {
-            question: 'What symbol signifies a class?',
-            choice1: '.',
-            choice2: '#',
-            choice3: ',',
-            choice4: '\'',
-            answer: 1,
-      },
-      {
-            question: 'What symbol is used to increment by 1?',
-            choice1: '--',
-            choice2: '==',
-            choice3: '||',
-            choice4: '++',
-            answer: 4,
-      },
-      {
-            question: 'What does \"true\" translate to if it is made a number?',
-            choice1: '1',
-            choice2: '0',
-            choice3: '2',
-            choice4: '-1',
-            answer: 1,
-      } 
+{
+      question: 'What is \"\{\" called?',
+      choice1: 'Bracket',
+      choice2: 'Curly bracket',
+      choice3: 'Comma',
+      choice4: 'Parentheses',
+      answer: 2,
+},
+{
+      question: 'What symbol signifies a class?',
+      choice1: '.',
+      choice2: '#',
+      choice3: ',',
+      choice4: '\'',
+      answer: 1,
+},
+{
+      question: 'What symbol is used to increment by 1?',
+      choice1: '--',
+      choice2: '==',
+      choice3: '||',
+      choice4: '++',
+      answer: 4,
+},
+{
+      question: 'What does \"true\" translate to if it is made a number?',
+      choice1: '1',
+      choice2: '0',
+      choice3: '2',
+      choice4: '-1',
+      answer: 1,
+} 
 ]
 
 const SCORE_POINTS = 100
 const MAX_QUESTIONS = 4
 
-// question operator
+
 startGame = () => {
-      questionCounter = 0
-      score = 0
-      availableQuestions = [...questions]
-      getNewQuestion()
+questionCounter = 0
+score = 0
+availableQuestions = [...questions]
+getNewQuestion()
 }
 
+var countDownDate = new Date(new Date().getTime() + 60000);
+timer = setInterval(timer_function , 1000)
+function timer_function() {
+var current_time = new Date().getTime();
+var time_left = countDownDate - current_time;
+var minutes = Math.floor((time_left % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((time_left % (1000 * 60)) / 1000);
+document.getElementById("minutes").innerHTML = minutes;
+document.getElementById("seconds").innerHTML = seconds;
+if (minutes < 1 && seconds == 0){
+      clearInterval(timer)
+      alert("Out of time")
+      localStorage.setItem("mostRecentScore", score);
+      window.location.href = "end.html"
+}
+}
+
+
+
+// question operator
 getNewQuestion = () => {
-      // keeps track of score
-      if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-            localStorage.setItem('mostRecentScore', score)
+// keeps track of score
+if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+      localStorage.setItem('mostRecentScore', score)
 
-            return window.location.assign('/end.html')
-      }
-      // question increment by 1 out of 4, then 2 out of 4, etc.
-      questionCounter++
-      progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-      // calculates current question and corresponds that with the percentage of how far through the quiz the user is
-      progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
-      // calculate value of question index
-      const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-      // keeping track of question user is on
-      currentQuestion = availableQuestions[questionsIndex]
-      // which question to ask
-      question.innerText = currentQuestion.question
-      // registers choices clicked
-      choices.forEach(choice => {
-            const number = choice.dataset['number']
-            choice.innerText = currentQuestion['choice' + number]
-      })
+      return window.location.assign('/end.html')
+}
+// question increment by 1 out of 4, then 2 out of 4, etc.
+questionCounter++
+progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+// calculates current question and corresponds that with the percentage of how far through the quiz the user is
+progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+// calculate value of question index
+const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+// keeping track of question user is on
+currentQuestion = availableQuestions[questionsIndex]
+// which question to ask
+question.innerText = currentQuestion.question
+// registers choices clicked
+choices.forEach(choice => {
+      const number = choice.dataset['number']
+      choice.innerText = currentQuestion['choice' + number]
+})
 
-      availableQuestions.splice(questionsIndex, 1) 
+availableQuestions.splice(questionsIndex, 1) 
 
-      acceptingAnswers = true
+acceptingAnswers = true
 
 }
 
 choices.forEach(choice => {
-      choice.addEventListener('click', e => {
-            if(!acceptingAnswers) return
+choice.addEventListener('click', e => {
+      if(!acceptingAnswers) return
 
-            acceptingAnswers = false
-            const selectedChoice = e.target
-            const selectedAnswer = selectedChoice.dataset['number']
+      acceptingAnswers = false
+      const selectedChoice = e.target
+      const selectedAnswer = selectedChoice.dataset['number']
 
-            let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' :'incorrect'
-            // adds points if selected answer is correct
-            if(classToApply === 'correct') {
-                  incrementScore(SCORE_POINTS)
-            }
-            // only adds points if it is correct
-            selectedChoice.parentElement.classList.add(classToApply)
-            // allows user some time to see if choice is right or wrong before moving onto next question
-            setTimeout(() => {
-                  selectedChoice.parentElement.classList.remove(classToApply)
-                  getNewQuestion()
-            }, 1000)
-      })
+      let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+      // adds points if selected answer is correct
+      if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+      }
+      selectedChoice.parentElement.classList.add(classToApply)
+      console.log(selectedChoice.parentElement.classList)
+      // allows user some time to see if choice is right or wrong before moving onto next question
+      setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+      }, 1000)
+})
 })
 
 incrementScore = num => {
-      score +=num
-      scoreText.innerText = score
+score +=num
+scoreText.innerText = score
 }
+
 
 startGame()
