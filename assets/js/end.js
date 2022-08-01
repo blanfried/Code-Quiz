07c682ad97eceb9e4e149d8e289da86951cb5a9c
Baer Lanfried <blanfried@yahoo.com>
2022-08-01 16:54:23 +0100

    end.html was not loading whilst live server was deactivated, I fixed this problem and also fixed the saving issue where the highscores.html page was also not loading

--- assets/js/end.js
+++ assets/js/end.js
@@ -31,7 +31,7 @@ saveHighScore = e => {
       highScores.splice(5)
 
       localStorage.setItem('highScores', JSON.stringify(highScores))
-      window.location.assign('/')
+      window.location.assign('/Users/baerlanfried/Code-Quiz/highscores.html')
 
       
 }
